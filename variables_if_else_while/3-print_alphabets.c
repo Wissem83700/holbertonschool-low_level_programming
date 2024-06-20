bd1bf86f3c6923ed5ea1d051c4cce717eb0d2e75
Wissem83700 <habarekwissem@gmail.com>
2024-06-20 08:38:10 +0000

    a

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,20 +3,24 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints the alphabet in lowercase,
- *              then in uppercase, followed by a new line.
+ * Description:
+ * Prints the alphabet in lowercase,
+ * then in uppercase, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return:
+ * Always 0 (Success)
  */
+int main(void)
+{
+	char c;
 
-int main(void) {
-char c;
-for (c = 'a'; c <= 'z'; c++) {
-putchar(c);
-}
-for (c = 'A'; c <= 'Z'; c++) {
-putchar(c);
-}
-putchar('\n');
-return 0;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
+	putchar('\n'); /* Print a new line */
+
+	return (0); /* Return statement with parentheses */
 }
