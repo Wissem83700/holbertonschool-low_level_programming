005b715cf62228b8c9c9642fff6d1db13880cf39
Wissem83700 <habarekwissem@gmail.com>
2024-06-20 12:48:30 +0000

    a

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - coucou
+  * Return: 0
+  */
+
+int main(void)
+{
+    char number;
+
+    for (number = '0'; number <= '9'; number++)
+    {
+        putchar(number);
+    }
+    putchar('\n');
+
+    return (0);
+}
